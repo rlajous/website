ebf69a32aadc68a1c5aff26929477fa47b9d7df2
rlajous <rodrigo@lajous.com.ar>
2024-03-30 16:48:28 -0300

    [ Style ] Changed experience card style

--- app/experience/components/Experience.tsx
+++ app/experience/components/Experience.tsx
@@ -18,18 +18,21 @@ const Experience: React.FC<ExperienceProps> = ({
 }) => {
   return (
     <div className="bg-white text-black dark:bg-gray-700 dark:text-white p-6 rounded-lg shadow-lg flex flex-col md:flex-row">
-      <div className="md:w-1/4 pr-4 border-r">
+      <div className="md:w-1/4 pr-4 border-r hidden md:block">
         <h2 className="text-xl font-semibold">{period}</h2>
       </div>
       <div className="md:w-3/4 pl-4">
         <h3 className="text-lg font-semibold">
           {position} · {company}
         </h3>
-        {responsibilities.map((item, itemIndex) => (
-          <p key={itemIndex} className="mt-2 text-sm">
-            {item}
-          </p>
-        ))}
+        <p className="text-gray-600 dark:text-gray-300 smd:hidden">{period}</p>
+        <ul className="list-disc ml-5 space-y-1">
+          {responsibilities.map((item, itemIndex) => (
+            <li key={itemIndex} className="mt-2 text-sm">
+              {item}
+            </li>
+          ))}
+        </ul>
         <div className="flex flex-wrap gap-2 mt-3">
           {technologies.map((tech, techIndex) => (
             <Badge key={techIndex} variant="secondary">
