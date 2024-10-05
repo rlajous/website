edd6eac02f7329a4075bb4e19a9a8e7c50b383d9
rlajous <rodrigolajous@gmail.com>
2024-10-05 18:18:54 -0300

    [ Refactor ] Contact Page styles, modularity and links fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,35 +1,26 @@
-import Link from "next/link";
-import { LinkedinIcon, MailIcon } from "lucide-react";
-import ContactForm from "./components/ContactForm";
+import { ContactForm } from "./components/ContactForm";
+import { EmailLink } from "./components/EmailLink";
+import { LinkedInLink } from "./components/LinkedInLink";
 
-export default function Component() {
+export default function ContactPage() {
+  const linkedInProfile = "rodrigo-manuel-navarro-lajous";
+  const email = "rodrigolajous@gmail.com";
+
   return (
-    <div className="flex flex-col items-center justify-center row-span-8 lg:row-span-11">
-      <main className="w-full max-w-2xl p-4 space-y-8">
-        <h1 className="text-4xl font-bold text-center text-gray-800 dark:text-gray-200">
+    <div className="flex flex-col items-center justify-center row-span-8 lg:row-span-11 w-full px-4 py-4 lg:py-6">
+      <main className="w-full max-w-2xl p-6 space-y-6 bg-white rounded-lg shadow-lg dark:bg-gray-800">
+        <h1 className="text-3xl font-bold text-center text-gray-800 dark:text-gray-200">
           Contact Me
         </h1>
-        <p className="text-lg text-center text-gray-600 dark:text-gray-400">
+        <p className="text-md text-center text-gray-600 dark:text-gray-400">
           Have a project you&apos;d like to discuss? Feel free to reach out.
         </p>
+
         <ContactForm />
-        <div className="flex items-center space-x-4 flex-col">
-          <Link
-            className="flex items-center mt-4 text-blue-600 dark:text-blue-400"
-            href="#"
-          >
-            <LinkedinIcon className="h-6 w-6 mr-2" />
-            /rodrigo-lajous
-            <span className="sr-only">LinkedIn</span>
-          </Link>
-          <a
-            className="flex items-center mt-4 text-gray-600 dark:text-gray-400"
-            href="mailto:odrigo@lajous.com.ar"
-          >
-            <MailIcon className="h-6 w-6 mr-2" />
-            rodrigolajous@gmail.com
-            <span className="sr-only">Email</span>
-          </a>
+
+        <div className="flex flex-col items-center space-y-4">
+          <LinkedInLink profile={linkedInProfile} />
+          <EmailLink email={email} />
         </div>
       </main>
     </div>
